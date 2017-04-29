8610596f46afcffc157f53ca4e9434bd746c092e
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2017-04-29 14:21:21 +0200

    Username and password are now in pack configuration

--- actions/get_projects.py
+++ actions/get_projects.py
@@ -6,7 +6,11 @@ class GetProjects(Action):
     def run(self):
         requests.packages.urllib3.disable_warnings()
 
-        url = 'https://' + str(self.config['tuleap_domain_name']) + '/api/v1/projects'
-        request_get_projects = requests.get(url, verify=False)
+        tuleap_domain_name = self.config['tuleap_domain_name']
+        username = self.config['tuleap_username']
+        password = self.config['tuleap_password']
+
+        url = 'https://' + str(tuleap_domain_name) + '/api/v1/projects'
+        request_get_projects = requests.get(url, auth=(username, password), verify=False)
 
         print(request_get_projects.text)
